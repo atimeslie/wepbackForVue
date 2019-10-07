a5171df366dbe833070f941bcb1e9cd2840c2991
atimeslie <2019546017@qq.com>
2019-10-07 22:35:03 +0800

    add project
    Signed-off-by: atimeslie <2019546017@qq.com>

--- src/app.vue
+++ src/app.vue
@@ -1,14 +1,26 @@
 <template>
-    <div id="app">{{ test }}</div>
+    <div id="app">{{ test }} {{name}}</div>
 </template>
 
 <script>
 /* eslint-disable */
+import {  fromEvent } from 'rxjs'
+import {take} from 'rxjs/operators'
+import { Observable } from 'rxjs'
+
   export default {
     data() {
       return {
-        test:'vueDemo'
+        test:'vueDemoq',
+        name: new Observable.of('张三')
       }
+    },
+    mounted(){
+      fromEvent(document,'click').pipe(
+        take(3)//Observable 只发出源 Observable 最初发出的的3个值
+      ).subscribe(res => {
+        console.log(11)
+      })
     }
   }
 /* eslint-disable */
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import app from './app.vue'
+import VueRx from 'vue-rx'
+import Rx from 'rxjs/Rx'
+
+Vue.use(VueRx, Rx)
 
 /* eslint-disable */
 new Vue({
